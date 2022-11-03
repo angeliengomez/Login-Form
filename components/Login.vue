c7d28d9887c8070b92059e76d4fa861afa287558
Angelien Gomez <gomezangelien@gmail.com>
2022-11-03 14:40:04 +0800

    CRUD Update

--- components/Login.vue
+++ components/Login.vue
@@ -1,80 +1,100 @@
 <template>
-<section class="vh-200" style="background-image: url('/image/samplebgimage.gif')">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-xl-10">
-        <div class="card" style="border-radius: 1rem; background-color: gray;">
-          <div class="row g-0">
-            <div class="col-md-6 col-lg-5 d-none d-md-block">
-                
-              <img src="/image/bg12.gif"
-               
-                alt="login form" class="img-fluid" style="border-radius: 1rem 1rem 0 1rem; margin-left: 20px; margin-top: 180px; size: contain;" />
-            </div>
-          
-          
-            
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
+  <section class="vh-200" style="background-image: url('/image/samplebgimage.gif')">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 1rem; background-color: gray;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
 
-                <form>
+                <img src="/image/bg12.gif" alt="login form" class="img-fluid"
+                  style="border-radius: 1rem 1rem 0 1rem; margin-left: 20px; margin-top: 180px; size: contain;" />
+              </div>
 
-                  <div class="d-flex align-items-center mb-3 pb-1">
-                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                    <span class="h1 fw-bold mb-0" style="font-family: 'Press Start 2P'; color:#F8C8DC; font-size: 50px; margin-left: 30px; margin-top: 5px">WELCOME!</span>
-                  </div>
-                  <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="/image/cutie1.gif"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 1rem 0 1rem; margin-left: -530px; margin-top: -300px; size: contain; " />
-            </div>
-                  <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="/image/cutie.gif"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 1rem 0 1rem; margin-left: 380px; margin-top: -380px; size: contain; " />
-            </div>
 
-                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px; font-family: 'Space Grotesk';  color:  #cfcfc4 ">Sign in to your account :'> </h5>
 
-                  <div class="form-outline mb-4">
-                    <input type="email" id="form2Example17" class="form-control form-control-lg"  style="animation-name: sample; animation-duration: 4s; animation-iteration-count: infinite; animation-direction: reverse;"/>
-                    <label class="form-label" for="form2Example17" style="font-family: 'Space Grotesk'; color:#F8C8DC; margin-top: 10px">Email address</label>
-                  </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
 
-                  <div class="form-outline mb-4">
-                    <input type="password" id="form2Example27" class="form-control form-control-lg" style="animation-name: sample; animation-duration: 4s; animation-iteration-count: infinite; animation-direction: reverse;" />
-                    <label class="form-label" for="form2Example27" style="font-family: 'Space Grotesk'; color:#cfcfc4; margin-top: 10px">Password</label>
-                  </div>
+                  <form>
 
-                  <div class="pt-1 mb-4">
-                    <button class="btn btn-secondary btn-lg btn-block" type="submit"  style="font-family: 'Press Start 2P'; color:#F8C8DC; animation-name: sample1; animation-duration: 10s; animation-iteration-count: infinite; animation-direction: reverse; ">
-                      <NuxtLink to="/Home" style="color:#F8C8DC">
-                        LOGIN
-                    </NuxtLink>
-                    </button>
-                 
-                  </div>
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                      <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                      <span class="h1 fw-bold mb-0"
+                        style="font-family: 'Press Start 2P'; color:#F8C8DC; font-size: 50px; margin-left: 30px; margin-top: 5px">WELCOME!</span>
+                    </div>
+                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                      <img src="/image/cutie1.gif" alt="login form" class="img-fluid"
+                        style="border-radius: 1rem 1rem 0 1rem; margin-left: -530px; margin-top: -300px; size: contain; " />
+                    </div>
+                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                      <img src="/image/cutie.gif" alt="login form" class="img-fluid"
+                        style="border-radius: 1rem 1rem 0 1rem; margin-left: 380px; margin-top: -380px; size: contain; " />
+                    </div>
 
-                  <a class="fw-normal mb-3 pb-3" href="#!" style="font-family: 'Space Grotesk'; color: #cfcfc4; font-size: 18px">Forgot password?</a>
-                </form>
+                    <h5 class="fw-normal mb-3 pb-3"
+                      style="letter-spacing: 1px; font-family: 'Space Grotesk';  color:  #cfcfc4 ">Sign in to your
+                      account :'> </h5>
 
+                    <div class="form-outline mb-4">
+                      <input type="email" id="form2Example17" class="form-control form-control-lg"
+                        style="animation-name: sample; animation-duration: 4s; animation-iteration-count: infinite; animation-direction: reverse;" />
+                      <label class="form-label" for="form2Example17"
+                        style="font-family: 'Space Grotesk'; color:#F8C8DC; margin-top: 10px">Email address</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                      <input type="password" id="form2Example27" class="form-control form-control-lg"
+                        style="animation-name: sample; animation-duration: 4s; animation-iteration-count: infinite; animation-direction: reverse;" />
+                      <label class="form-label" for="form2Example27"
+                        style="font-family: 'Space Grotesk'; color:#cfcfc4; margin-top: 10px">Password</label>
+                    </div>
+
+                    <div class="pt-1 mb-4">
+                      <button class="btn btn-secondary btn-lg btn-block" type="submit"
+                        style="font-family: 'Press Start 2P'; color:#F8C8DC; animation-name: sample1; animation-duration: 10s; animation-iteration-count: infinite; animation-direction: reverse; ">
+                        <NuxtLink to="/Home" style="color:#F8C8DC">
+                          LOGIN
+                        </NuxtLink>
+                      </button>
+
+                    </div>
+
+                    <a class="fw-normal mb-3 pb-3" href="#!"
+                      style="font-family: 'Space Grotesk'; color: #cfcfc4; font-size: 18px">Forgot password?</a>
+                  </form>
+
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 </template>
 <style>
-section{
-    background-image: url("/image/background.gif");
+section {
+  background-image: url("/image/background.gif");
 }
-@keyframes sample{
-    from {background-color: white;}
-  to {background-color: #F8C8DC;}
+
+@keyframes sample {
+  from {
+    background-color: white;
+  }
+
+  to {
+    background-color: #F8C8DC;
+  }
 }
-@keyframes sample1{
-    from {background-color: white;}
-  to {background-color: #302c2d;}
+
+@keyframes sample1 {
+  from {
+    background-color: white;
+  }
+
+  to {
+    background-color: #302c2d;
+  }
 }
 </style>
--- components/StudentList.vue
+++ components/StudentList.vue
@@ -1,100 +1,172 @@
 <template>
-    <section class="vh-600" style="background-image: url('/image/samplebgimage.gif')">
-      <div class="container py-5 h-200">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col col-xl-10">
-            <div class="card" style="border-radius: 1rem;">
-              <div class="row g-0">
+  <section class="vh-600" style="background-image: url('/image/samplebgimage.gif')">
+    <div class="container py-5 h-200">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+
+
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                    <span class="h1 fw-bold mb-0"
+                      style="font-family: 'Press Start 2P'; color: black; font-size: 50px; margin-left: 300px; margin-top: 5px; align-items: center;">Student
+                      List</span>
+                  </div>
+
+
+
+                  <div class="col-md-6 col-lg-4 d-none d-md-block">
+                    <img src="/image/cutie.gif" alt="pic1" class="img-fluid"
+                      style="border-radius: 1rem 1rem 0 1rem; margin-left: -30px; margin-top: -200px; size: contain; " />
+                  </div>
+                  <div>
+                    <b-table striped hover :items="students" :fields="fields"
+                      style="color:black; font-family: Space Mono; margin-left: 100px; font-size: 20px; text-align:center ;">
+                      <template v-slot:cell()="{ value, item, field: { key }}">
                 
-                <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                  <div class="card-body p-4 p-lg-5 text-black">
-    
-                  
-    
-                      <div class="d-flex align-items-center mb-3 pb-1">
-                        <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                        <span class="h1 fw-bold mb-0" style="font-family: 'Press Start 2P'; color: black; font-size: 50px; margin-left: 300px; margin-top: 5px; align-items: center;">Student List</span>
+                <template v-if="!item.isEditing">{{ value }}</template>
+              <b-form-input v-else v-model="item[key]" />
+            </template>
+                      <template v-slot:cell(buttons)="{ item }">
+
+                        <div class="row">
+                          <div class="col">
+                            <button @click="removeStudent(item)" style="font-family: 'Press Start 2P'">X</button>
+                            <b-button @click="editStudent(item)" v-b-modal.mymodal style="font-family: 'Press Start 2P'; margin-left: 10px;">E</b-button>
+                          </div>
+
+                        </div>
+                        
+                      </template>
+
+
+                    </b-table>
+                    <form @submit.prevent="addStudent">
+
+                      <input v-model="newStudentlname" placeholder="Lastname" style="margin-left: 20px; font-family: 'Space Mono';">
+                      <input v-model="newStudentfname" placeholder="Firstname" style="margin-left:5px; margin-right: 45px; margin-top: 20px; font-family: 'Space Mono';">
+                      <input v-model="newStudentcourse" placeholder="Course" style="margin-left: -40px; font-family: 'Space Mono';">
+
+                      <button
+                        style="margin-left: 300px; margin-top: 20px; margin-bottom: 20px; font-family: 'Press Start 2P' ">ADD
+                        STUDENT</button>
+                     
+
+                      <div>
+<!-- 
+
+                        <b-modal hide-footer id="mymodal" ref="modal"
+                          title="Update Student Information">
+                          <form ref="form" @submit.stop.prevent="handleSubmit">
+                            <b-form-group label="First Name" label-for="name-input"
+                              invalid-feedback="Information is required" :state="nameState">
+                              <b-form-input id="name-input" v-model="fname" :state="nameState" required></b-form-input>
+                            </b-form-group>
+                            <b-form-group label="Last Name" label-for="name-input"
+                              invalid-feedback="Information is required" :state="nameState">
+                              <b-form-input id="name-input" v-model="lname" :state="nameState" required></b-form-input>
+                            </b-form-group>
+                            <b-form-group label="Course" label-for="name-input"
+                              invalid-feedback="Information is required" :state="nameState">
+                              <b-form-input id="name-input" v-model="course" :state="nameState" required></b-form-input>
+                            </b-form-group>
+                            <button @click="editStudent(item)"  style="background-color:gray; font-family: 'Press Start 2P'; color: white">Update</button>>
+                          </form>
+                        </b-modal>
+                        -->
                       </div>
 
-    
-                      <div class="col-md-6 col-lg-4 d-none d-md-block">
-              <img src="/image/cutie.gif"
-                alt="pic1" class="img-fluid" style="border-radius: 1rem 1rem 0 1rem; margin-left: -30px; margin-top: -200px; size: contain; " />
-            </div>
-            <div>
-              <b-table striped hover :items="students" style="color:black; font-family: 'Space Mono'; margin-left: 100px; font-size: 20px; text-align:center ;"></b-table>
-  <form @submit.prevent="addStudent">
 
-  <input v-model="newStudent" style="margin-left: 200px; font-family: 'Space Mono';">
-  <button style="margin-left: 50px; margin-top: 20px; margin-bottom: 20px; font-family: 'Press Start 2P' ">ADD STUDENT</button>   
-  <ul>
-    <li v-for="student in students" :key="student.id" style="margin-left:300px; font-family: 'Space Mono'; color: black; font-size: 16px ">
-    {{student.Information}}
-    <button @click="removeStudent(student)">X</button>
-    </li>
-    </ul>> 
+                    </form>
 
-  </form>
-    
 
-  </div>
-
-  
-    
-
-      
-    
-  </div>
-    
-                      
-    
-                
-                  
-    
                   </div>
+
+
+
+
+
+
                 </div>
+
+
+
+
+
+
               </div>
             </div>
           </div>
         </div>
-    
-    </section>
-    </template>
-    <style>
-    section{
-        background-image: url("/image/background.gif");
-    }
-    
-    </style>
-    <script>
-    let id = 0
-    export default {
-      data() {
-        return {
-          newStudent:'',
-          students: [
-         {id: id++, Information: 'Sample Student Name, BSIT'},
-         {id: id++, Information: 'Sample Student Name1, BSIT'},
-         {id: id++, Information: 'Sample Student Name2, BSIT'}
-        ]
+      </div>
+    </div>
 
-        }
-        
-      },
-      methods:{
-        addStudent(){
-          
-          this.students.push({id: id++, Information: this.newStudent});
-          this.newStudent = ''
-        },
-        removeStudent(student)
-        {
-          this.students = this.students.filter((t) => t !== student)
-        }
-      }
-     
-    
+  </section>
+</template>
+<style>
+section {
+  background-image: url("/image/background.gif");
+}
+</style>
+<script>
+let id = 0
+export default {
+  data() {
+    return {
+      newStudent: '',
+      fields: ['id', 'LastName', 'FirstName', 'Course', 'buttons'],
+      students: [
+        { id: id++, LastName: 'Gomez', FirstName: 'Angelien', Course: 'BSIT' },
+        { id: id++, LastName: 'Gomez', FirstName: 'Hatdog', Course: 'BSIT' },
+        { id: id++, LastName: 'Gomez', FirstName: 'Sample', Course: 'BSIT' },
+
+      ]
+
     }
-  </script>
+
+  },
+  methods: {
+    addStudent() {
+
+      this.students.push({ id: id++, LastName: this.newStudentlname, FirstName: this.newStudentfname, Course: this.newStudentcourse });
+      this.newStudentlname = "";
+      this.newStudentfname = "";
+      this.newStudentcourse = "";
+    },
+    removeStudent(item) {
+      this.students = this.students.filter((t) => t !== item);
+    },
+    updateStudent() {
+      this.students = this.students.splice({ id, LastName: this.newStudentlname, FirstName: this.newStudentfname, Course: this.newStudentcourse});
+      this.newStudent = ''
+    },
+    editStudent(item)
+    {
+      if(item.isEditing)
+                {
+                    this.$set(item, 'isEditing', false);
+                   
+                }
+                else
+                {
+                    this.$set(item, 'isEditing', true);
+                    
+                }
+    }
+  
+   
+   
+
+  }
+
+
+
+}
+</script>
  
     
